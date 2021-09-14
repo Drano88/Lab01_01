6cb06e32f256790322229eba2f9538a11e6f440c
itlabs <itlabs@StudentGrid02.it.farmingdale.edu>
2021-09-13 21:21:23 -0400

    commit message via miguel

---
+++ Lab01/src/main/java/com/mycompany/lab01/NewClass.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.lab01;
+
+
+public class NewClass {
+    public static void main(String args[]) {
+		// Print Statment 
+    System.out.println("\nEven Numbers 0 - 100 : ");
+    
+    // Loop starting at 0 to include 0 and less than or equal 100 so that it includes 100 		
+		for (int i=0; i<=100; i++) {
+    // if value of i is divisable by 2 w/ no remainder thus making it an even number
+			if (i%2==0)
+
+  // This statment below prints the value of i and adds a break line 
+		  System.out.print(i + "\n");	
+      
+        }			
+      }
+    }
